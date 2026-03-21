fbfa44662e3f6ec7c3c48e41341dde059c59042b
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-21 13:20:23 +0530

    feat: add CORS middleware to enable cross-origin requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 
 const productRoute = require("./routes/product.route");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
