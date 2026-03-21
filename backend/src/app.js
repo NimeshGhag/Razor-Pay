354f1cde012cda8083d0d035071b8c1cba5c9a16
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-21 12:43:26 +0530

    feat: set up product routes and integrate with Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require("express");
+
+const productRoute = require("./routes/product.route");
+
 const app = express();
+
+app.use("/api/products", productRoute);
 
 app.use(express.json());
 
