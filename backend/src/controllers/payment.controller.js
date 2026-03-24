d9a781dd882b241865d389d09d7ad9cbf248c6d1
NimeshGhag <ghagnimesh490@gmail.com>
2026-03-24 11:37:07 +0530

    feat: implement createOrder function for processing payments

--- backend/src/controllers/payment.controller.js
+++ backend/src/controllers/payment.controller.js
@@ -1,7 +1,39 @@
-const Razorpay = require('razorpay');
+const Razorpay = require("razorpay");
+const paymentModel = require("../models/payment.model");
+const productModel = require("../models/product.model");
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
+const createOrder = async (req, res) => {
+  const product = await productModel.findOne();
+
+  const options = {
+    amount: product.price.amount,
+    currency: product.price.currency,
+  };
+
+  try {
+    const order = await razorpay.orders.create(options);
+    const newPayment = await paymentModel.create({
+      orderId: order.id,
+      price: options,
+      status: "PENDING",
+    });
+    res.status(201).json({
+      message: "Order created successfully",
+      order,
+      payment: newPayment,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createOrder,
+};
